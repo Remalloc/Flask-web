9bff0a1882a4202b1663af16f41d94259b3620b3
vbover <vbover@163.com>
2017-07-30 10:29:56 +0800

    [add,modify] new templates

--- hello.py
+++ hello.py
@@ -1,10 +1,47 @@
 #coding = utf-8
-from  flask import Flask
-app=Flask(__name__)
+from  flask import Flask,render_template,redirect,session,url_for,flash
+from flask_bootstrap import Bootstrap
 
-@app.route('/')
+from flask_moment import Moment
+from datetime import datetime
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+app=Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
+Bootstrap(app)
+Moment(app)
+
+class NameForm(FlaskForm):
+ name = StringField('What is your name?', validators=[DataRequired()])
+ submit = SubmitField('Submit')
+
+@app.route('/',methods=['GET','POST'])
 def index():
-    return '<h1>Hello World</h1>'
+    form=NameForm()
+    if form.validate_on_submit():
+        old_name=session.get('name')
+        if old_name is not None and old_name!=form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name']=form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html',form=form,name=session.get('name'))
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html',name=name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'),404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'),500
+
+
 
 if __name__=='__main__':
-    app.run(debug=True)
+    app.run()
